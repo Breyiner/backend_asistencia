516e1c1f8c36e79b5aecc8374c2589a922a13009
Breyiner <breyneracosta7@gmail.com>
2026-01-08 10:54:55 -0500

    se implementa seeder para los trimestres de las fichas

---
+++ database/seeders/FichaTermSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\FichaTerm;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class FichaTermSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $fichaTerms = [
+
+            [
+                "term_id" => 1,
+                "ficha_id" => 1,
+                "phase_id" => 2,
+                "start_date" => "2024-12-13",
+                "end_date" => "2025-03-13",
+                "is_active" => true
+            ],
+
+        ];
+
+        foreach ($fichaTerms as $fichaTerm) {
+            FichaTerm::create($fichaTerm);
+        }
+    }
+}
